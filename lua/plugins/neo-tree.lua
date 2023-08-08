fd8b82b25671d4afb199cf2c74752418e9445f69
Paul Bennett <pbennett.uk@gmail.com>
2023-08-08 19:05:02 +0100

    adding show hidden files by default

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+-- simple change to show hidden files by default 
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  }
