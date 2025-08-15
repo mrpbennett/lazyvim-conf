96a2c2095b0846f34ba954ddffff5e4e0a27c7cc
Paul Bennett <p@mrpbennett.dev>
2025-08-15 15:13:58 +0100

    tweaks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.wrap = true
-vim.g.codeium_os = "Darwin"
-vim.g.codeium_arch = "arm64"
+-- vim.opt.wrap = true
+-- vim.g.codeium_os = "Darwin"
+-- vim.g.codeium_arch = "arm64"
 
 -- 🔧 Global indentation settings (4 spaces)
-vim.opt.tabstop = 4      -- Number of spaces that a <Tab> counts for
-vim.opt.softtabstop = 4  -- Number of spaces inserted/deleted with <Tab>/<BS>
-vim.opt.shiftwidth = 4   -- Number of spaces for autoindent
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> counts for
+vim.opt.softtabstop = 4 -- Number of spaces inserted/deleted with <Tab>/<BS>
+vim.opt.shiftwidth = 4 -- Number of spaces for autoindent
 vim.opt.expandtab = true -- Use spaces instead of tabs
 
 -- no comments on new lines
