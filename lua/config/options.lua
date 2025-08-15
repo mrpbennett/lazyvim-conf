fdc1ff070daaa67b3301a0b050ea07475d7b59b4
Paul Bennett <pbennett.uk@gmail.com>
2025-08-15 13:32:36 +0100

    Refactor LSP and formatting configurations: update Python LSP from pylsp to pyright, and remove deprecated LSP and none-ls plugin configurations.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,9 +6,9 @@ vim.g.codeium_os = "Darwin"
 vim.g.codeium_arch = "arm64"
 
 -- 🔧 Global indentation settings (4 spaces)
-vim.opt.tabstop = 4 -- Number of spaces that a <Tab> counts for
-vim.opt.softtabstop = 4 -- Number of spaces inserted/deleted with <Tab>/<BS>
-vim.opt.shiftwidth = 4 -- Number of spaces for autoindent
+vim.opt.tabstop = 4      -- Number of spaces that a <Tab> counts for
+vim.opt.softtabstop = 4  -- Number of spaces inserted/deleted with <Tab>/<BS>
+vim.opt.shiftwidth = 4   -- Number of spaces for autoindent
 vim.opt.expandtab = true -- Use spaces instead of tabs
 
 -- no comments on new lines
@@ -19,7 +19,7 @@ vim.g.lazyvim_eslint_auto_format = true
 vim.g.lazyvim_prettier_needs_config = false
 
 -- 🐍 PYTHON
-vim.g.lazyvim_python_lsp = "pylsp"
+vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
 
 -- Invert the cursor
